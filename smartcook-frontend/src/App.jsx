1c8783b3c9c1464d220ef59d442df13fa5ad0111
DinThie32 <dinhthien1159@gmail.com>
2026-04-17 14:52:58 +0700

    feat: add AdminDashboard, Moderation, and AuditLogs pages

--- smartcook-frontend/src/App.jsx
+++ smartcook-frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ProfilePage from './pages/ProfilePage'; 
+import AdminDashBoardPage from './pages/AdminDashBoardPage';
+import ModerationPage from './pages/ModerationPage';
+import AuditLogsPage from './pages/AuditLogsPage';
 import Layout from "./components/Layout";
 import RecipePage from "./components/RecipeCard";
 
@@ -9,8 +13,18 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          
           <Route index element={<HomePage />} />
           <Route path="recipe/:id" element={<RecipePage />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="profile/:id" element={<ProfilePage />} />
+
+          <Route path="admin">
+            <Route index element={<AdminDashBoardPage />} />
+            <Route path="moderation" element={<ModerationPage />} />
+            <Route path="audit-logs" element={<AuditLogsPage />} />
+          </Route>
+
         </Route>
       </Routes>
     </BrowserRouter>
